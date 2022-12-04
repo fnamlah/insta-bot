b1712c7ce79441ae5bae2923005064b6ebe6529a
fnamlah <faisalmalnamlah@gmail.com>
2022-12-04 23:07:04 +0300

    final commit

--- main.py
+++ main.py
@@ -2,58 +2,61 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import ElementClickInterceptedException
-
 import time
-INSTAGRAM_USERNAME = "-"
-INSTAGRAM_PASSWORD = "-"
 
-chrome_driver_path = Service("-")
+CHROME_DRIVER_PATH = "-"
+SIMILAR_ACCOUNT = "coders.learning"
+USERNAME = "-"
+PASSWORD = "-"
+url = f"https://www.instagram.com/{SIMILAR_ACCOUNT}/"
 
 
 class InstaFollower:
-    def __init__(self, driver_path):
-        self.driver = webdriver.Chrome(service=driver_path)
+    def __init__(self):
+        self.service = Service(CHROME_DRIVER_PATH)
+        self.driver = webdriver.Chrome(service=self.service)
+        self.driver.maximize_window()
 
     def login(self):
-        self.driver.get("https://www.instagram.com/accounts/login/")
-        time.sleep(5)
-
+        self.driver.get(url)
+        time.sleep(3)
+        self.driver.find_element(By.CSS_SELECTOR, 'button[class="_acan _acap _acas"]').click()
+        time.sleep(3)
         username = self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')
-        username.send_keys(INSTAGRAM_USERNAME)
+        username.send_keys(USERNAME)
         password = self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input')
-        password.send_keys(INSTAGRAM_PASSWORD)
+        password.send_keys(PASSWORD)
         time.sleep(5)
 
         login_button = self.driver.find_element(By.XPATH, '//*[@id="loginForm"]/div/div[3]/button')
         login_button.click()
         time.sleep(5)
 
-        not_save = self.driver.find_element(By.XPATH, '//*[@id="mount_0_0_yn"]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div/button')
-        not_save.click()
-        time.sleep(5)
+        self.driver.find_element(By.CSS_SELECTOR, 'button[class="_acan _acao _acas"]').click()
+        time.sleep(3)
+        # self.driver.find_element(By.CSS_SELECTOR, 'button[class="_a9-- _a9_1"]').click()
 
     def find_followers(self):
-        self.driver.get("https://www.instagram.com/nike/")
-        time.sleep(3)
-
-        followers = self.driver.find_element(By.XPATH, '//*[@id="mount_0_0_5s"]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/header/section/ul/li[2]/a')
-        followers.click()
-        time.sleep(3)
+        followers = self.driver.find_elements(By.CSS_SELECTOR, '.x78zum5 li')
+        followers[1].click()
+        time.sleep(20)
+        pop_up = self.driver.find_element(By.CSS_SELECTOR, 'div[class="_aano"]')
+        for i in range(3):
+            self.driver.execute_script("arguments[0].scrollTop = arguments[0].scrollHeight", pop_up)
+        time.sleep(10)
 
     def follow(self):
-        self.driver.get("https://www.instagram.com/nike/followers/")
-        all_buttons = self.driver.find_elements(By.CSS_SELECTOR, "li button")
-        for button in all_buttons:
+        follow_buttons = self.driver.find_elements(By.CSS_SELECTOR, 'button[class="_acan _acap _acas"]')
+        for button in follow_buttons:
             try:
                 button.click()
-                time.sleep(1)
-            except ElementClickInterceptedException:
-                cancel_button = self.driver.find_element(By.XPATH, '/html/body/div[5]/div/div/div/div[3]/button[2]')
-                cancel_button.click()
+            except:
+                pass
+        print("Bot successful.")
 
 
-instagram = InstaFollower(chrome_driver_path)
-instagram.login()
-instagram.follow()
+insta_bot = InstaFollower()
+insta_bot.login()
+insta_bot.find_followers()
+insta_bot.follow()
 time.sleep(1000)
